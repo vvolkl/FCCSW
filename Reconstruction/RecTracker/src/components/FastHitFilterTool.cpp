8e8014cb919c5b5f0819287585550485290f4d5a
Valentin Volkl <valentin.volkl@cern.ch>
2018-05-08 11:10:31 +0200

    check format

--- Reconstruction/RecTracker/src/components/FastHitFilterTool.cpp
+++ Reconstruction/RecTracker/src/components/FastHitFilterTool.cpp
@@ -1,18 +1,12 @@
 #include "FastHitFilterTool.h"
 
-
 #include "datamodel/TrackHitCollection.h"
-
-
-
 
 DECLARE_TOOL_FACTORY(FastHitFilterTool)
 
-FastHitFilterTool::FastHitFilterTool(const std::string& type, const std::string& name,
-                                                   const IInterface* parent)
+FastHitFilterTool::FastHitFilterTool(const std::string& type, const std::string& name, const IInterface* parent)
     : GaudiTool(type, name, parent) {
   declareInterface<IHitFilterTool>(this);
-
 }
 
 StatusCode FastHitFilterTool::initialize() {
@@ -20,38 +14,28 @@ StatusCode FastHitFilterTool::initialize() {
   return sc;
 }
 
-bool   FastHitFilterTool::filter(fcc::TrackHit hit) {
+bool FastHitFilterTool::filter(fcc::TrackHit hit) {
 
-  
-  if (m_systemId == 0 || m_systemId == 1) { // barrel 
+  if (m_systemId == 0 || m_systemId == 1) {  // barrel
     if (hit.core().cellId % 16 == m_systemId) {
       if ((hit.core().cellId >> 4) % 32 == m_layerId) {
         return true;
       }
     }
-  } else { //endcap
+  } else {  // endcap
     if (hit.core().cellId % 16 == m_systemId) {
       if ((hit.core().cellId >> 4) % 64 == m_layerId) {
         return true;
       }
     }
-
-
   }
 
-
-      
- return false;
-
+  return false;
 }
 
 void FastHitFilterTool::setIds(unsigned int systemId, unsigned int layerId) {
   m_systemId = systemId;
   m_layerId = layerId;
-
 }
-
-
-
 
 StatusCode FastHitFilterTool::finalize() { return GaudiTool::finalize(); }
