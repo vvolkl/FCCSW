2be209aa0d053f527af73107f9bc660acad08142
Valentin Volkl <valentin.volkl@cern.ch>
2018-05-08 11:38:28 +0200

    Add comment

--- Reconstruction/RecTracker/src/components/RecTrackAlg.h
+++ Reconstruction/RecTracker/src/components/RecTrackAlg.h
@@ -38,11 +38,14 @@ public:
   StatusCode finalize() override final;
 
 private:
-  /// TrackHits as Input to the track seeding
+  /// TrackHits as input to the track seeding
   DataHandle<fcc::PositionedTrackHitCollection> m_positionedTrackHits{"TrackerPositionedHits", Gaudi::DataHandle::Reader,
                                                                       this};
+  /// Tracks as output of the algorithm
   DataHandle<fcc::TrackCollection> m_tracks{"Tracks", Gaudi::DataHandle::Writer, this};
+  /// Reconstructed particles as output of the algorithm
   DataHandle<fcc::ParticleCollection> m_recParticles{"TrackRecoParticles", Gaudi::DataHandle::Writer, this};
+  /// TrackStates as output of the algorithm
   DataHandle<fcc::TrackStateCollection> m_trackStates{"TrackStates", Gaudi::DataHandle::Writer, this};
   /// Handle to Track Seeding Tool that does the work
   ToolHandle<ITrackSeedingTool> m_trackSeedingTool{"CombinatorialSeedingTool/CombinatorialSeedingTool", this};
