f766dcf0e6e73c23a2feb879a95915ab1f854b3e
Valentin Volkl <valentin.volkl@cern.ch>
2018-06-14 12:05:05 +0200

    Add PodioInput to KalmanFilter options

--- Reconstruction/RecTracker/options/KalmanFilterTest.py
+++ Reconstruction/RecTracker/options/KalmanFilterTest.py
@@ -1,20 +1,31 @@
 from Gaudi.Configuration import *
 
 from Configurables import FCCDataSvc
-podioevent   = FCCDataSvc("EventDataSvc")
+podioevent   = FCCDataSvc("EventDataSvc", input="output_trkExtrapolationTest.root")
 
+from Configurables import PodioInput
+podioinput = PodioInput("PodioReader", 
+                        collections= [
+                                      "ExtrapolatedTrackstates",
+                                      "SimTrackerPositionedHits",
+                                      ],
+                          OutputLevel=DEBUG,
+                          )
 
 
 
 from Configurables import KalmanFilter
 kalman = KalmanFilter()
+kalman.FittedTracks.Path = "FittedTracks"
+kalman.TrackSeeds.Path = "ExtrapolatedTrackstates"
+kalman.TrackerHits.Path = "TrackerHits"
 
 
 from Configurables import ApplicationMgr
-ApplicationMgr( TopAlg = [kalman],
+ApplicationMgr( TopAlg = [podioinput, kalman],
                 EvtSel = 'NONE',
-                EvtMax   = 1,
+                EvtMax   = 10,
                 # order is important, as GeoSvc is needed by SimG4Svc
-                ExtSvc = [],
+                ExtSvc = [podioevent],
                 OutputLevel=DEBUG,
  )
