752fe6e0ec5cce61d7dfe5041ed33c85b379790f
Valentin Volkl <valentin.volkl@cern.ch>
2019-05-06 11:31:32 +0200

    SimG4Fast: Adapt momentum ranges

--- Examples/options/geant_fastsim.py
+++ Examples/options/geant_fastsim.py
@@ -8,17 +8,18 @@ podioevent = FCCDataSvc("EventDataSvc")
 
 
 ## Particle Gun
-from Configurables import MomentumRangeParticleGun
-guntool = MomentumRangeParticleGun()
-guntool.EtaMin=0
-guntool.EtaMax=6
-guntool.EnergyMin=0.01*units.GeV
-guntool.EnergyMax=10*units.Tev
-guntool.PdgCodes=[-211]
+from Configurables import ConstPtParticleGun
+guntool = ConstPtParticleGun()
+guntool.EtaMin = 0.0
+guntool.EtaMax = 5.
+guntool.PtMin = 0.01*units.GeV
+guntool.PtMax = 10*units.GeV
+guntool.PdgCodes = [-211]
 
 from Configurables import GenAlg
 gun = GenAlg()
 gun.hepmc.Path = "hepmc"
+gun.SignalProvider = guntool
 
 ## reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
 from Configurables import HepMCToEDMConverter
@@ -33,7 +34,8 @@ from Configurables import GeoSvc
 geoservice = GeoSvc("GeoSvc")
 geoservice.detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
                                          'file:Detector/DetFCChhBaseline1/compact/FCChh_TrackerAir.xml',
-                                         'file:Detector/DetFCChhECalSimple/compact/FCChh_ECalBarrel_Gflash.xml']
+                                         #'file:Detector/DetFCChhECalSimple/compact/FCChh_ECalBarrel_Gflash.xml'
+                                         ]
 geoservice.OutputLevel=INFO 
 
 # Geant4 service
@@ -52,17 +54,21 @@ from Configurables import SimG4FastSimTrackerRegion
 regiontooltracker = SimG4FastSimTrackerRegion()
 regiontooltracker.volumeNames=["TrackerEnvelopeBarrel"]
 regiontooltracker.smearing=smeartool
-## create parametrisation of the calorimeter
-from Configurables import SimG4GflashSamplingCalo
-gflash = SimG4GflashSamplingCalo()
-gflash.materialActive = "G4_lAr"
-gflash.materialPassive = "G4_Pb"
-gflash.thicknessActive = 4
-gflash.thicknessPassive = 2
-from Configurables import SimG4FastSimCalorimeterRegion
-regiontoolcalo = SimG4FastSimCalorimeterRegion()
-regiontoolcalo.volumeNames = ["ECalBarrel"]
-regiontoolcalo.parametrisation = gflash
+regiontooltracker.maxEta = 10
+regiontooltracker.maxMomentum = 10*units.TeV
+
+### create parametrisation of the calorimeter
+#from Configurables import SimG4GflashSamplingCalo
+#gflash = SimG4GflashSamplingCalo()
+#gflash.materialActive = "G4_lAr"
+#gflash.materialPassive = "G4_Pb"
+#gflash.thicknessActive = 4
+#gflash.thicknessPassive = 2
+#from Configurables import SimG4FastSimCalorimeterRegion
+#regiontoolcalo = SimG4FastSimCalorimeterRegion()
+#regiontoolcalo.volumeNames = ["ECalBarrel"]
+#regiontoolcalo.parametrisation = gflash
+
 ## create overlay on top of FTFP_BERT physics list, attaching fast sim/parametrization process
 from Configurables import SimG4FastSimPhysicsList
 physicslisttool = SimG4FastSimPhysicsList()
@@ -70,7 +76,10 @@ physicslisttool.fullphysics="SimG4FtfpBert"
 ## attach those tools to the G4 service
 geantservice = SimG4Svc()
 geantservice.physicslist=physicslisttool
-geantservice.regions=[regiontooltracker, regiontoolcalo]
+geantservice.regions = [
+                        regiontooltracker,
+                        #regiontoolcalo,
+                       ]
 
 # Geant4 algorithm
 # Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
@@ -82,18 +91,23 @@ from Configurables import SimG4SaveSmearedParticles
 saveparticlestool = SimG4SaveSmearedParticles()
 saveparticlestool.particles.Path = "SmearedParticles"
 saveparticlestool.particlesMCparticles.Path = "particleMCparticleAssociation"
+
 from Configurables import SimG4SaveCalHits
 savecaltool = SimG4SaveCalHits()
 savecaltool.readoutNames = ["ECalHitsPhiEta"]
 savecaltool.positionedCaloHits.Path = "CaloPositionedHits"
 savecaltool.caloHits.Path = "CaloHits"
+
 # next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
 from Configurables import SimG4PrimariesFromEdmTool
 particle_converter = SimG4PrimariesFromEdmTool()
 particle_converter.genParticles.Path = "GenParticles"
 from Configurables import SimG4Alg
 geantsim = SimG4Alg()
-geantsim.outputs = [saveparticlestool, savecaltool]
+geantsim.outputs = [
+                      saveparticlestool, 
+                      #savecaltool,
+                    ]
 geantsim.eventProvider=particle_converter
 
 from Configurables import SimG4FastSimHistograms
@@ -115,7 +129,7 @@ out.outputCommands = ["keep *"]
 from Configurables import ApplicationMgr
 ApplicationMgr( TopAlg = [gun, hepmc_converter, geantsim, hist, out],
                 EvtSel = 'NONE',
-                EvtMax   = 100,
+                EvtMax   = 200,
                 # order is important, as GeoSvc is needed by SimG4Svc
                 ExtSvc = [podioevent, geoservice, geantservice],
-                OutputLevel=INFO)
+                OutputLevel=DEBUG)
