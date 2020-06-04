464bf939edc6bd99226c1ac6458b599f53359d03
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-04 23:52:17 +0200

    up11

--- .github/scripts/gitlabmirror.sh
+++ .github/scripts/gitlabmirror.sh
@@ -24,7 +24,7 @@ shortbranch=$(git symbolic-ref --short HEAD)
 
 
 # need a token with api acces
-curl -X POST  --header "PRIVATE-TOKEN: ${GITLAB_PASSWORD2}" https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/mirror/pull
+curl --silent   --header "PRIVATE-TOKEN: ${GITLAB_PASSWORD2}" -X https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/mirror/pull
 
 sleep $POLL_TIMEOUT
 
