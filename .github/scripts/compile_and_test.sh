15a89239601c697fb6cf484ae392fc96d5cdbc17
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-30 16:44:40 +0200

    up

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -2,6 +2,8 @@
 
 cd /Package
 source init.sh
+ls /cvmfs/geant4.cern.ch/share/data/G4ENSDFSTATE2.2
+ls $G4ENSDFSTATEDATA
 mkdir build install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17  .. && \
