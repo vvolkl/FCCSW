f51a4759906e202001fd2bc063bfd999fa70f1ec
Valentin Volkl <valentin.volkl@cern.ch>
2020-07-12 19:50:03 +0200

    up68

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -5,9 +5,18 @@ source init.sh
 ls /cvmfs/geant4.cern.ch/share/data/G4ENSDFSTATE2.2
 ls $G4ENSDFSTATEDATA
 mkdir geant4data
-cp -r $G4ENSDFSTATEDATA/* ./geant4data; export G4ENSDFSTATEDATA=$PWD/geant4data; 
-cp -r $G4PARTICLEXSDATA/* ./geant4data; export G4PARTICLEXSDATA=$PWD/geant4data; 
-cp -r $G4LEDATA/* ./geant4data; export G4LEDATA=$PWD/geant4data; 
+cp  $G4ENSDFSTATEDATA/* ./geant4data; export G4ENSDFSTATEDATA=$PWD/geant4data; 
+cp  $G4PARTICLEXSDATA/* ./geant4data; export G4PARTICLEXSDATA=$PWD/geant4data; 
+export G4NEUTRONHPDATA=/cvmfs/geant4.cern.ch/share/data/G4NDL4.6
+export G4LEDATA=/cvmfs/geant4.cern.ch/share/data/G4EMLOW7.9.1
+export G4LEVELGAMMADATA=/cvmfs/geant4.cern.ch/share/data/PhotonEvaporation5.5
+export G4RADIOACTIVEDATA=/cvmfs/geant4.cern.ch/share/data/RadioactiveDecay5.4
+export G4PIIDATA=/cvmfs/geant4.cern.ch/share/data/G4PII1.3
+export G4REALSURFACEDATA=/cvmfs/geant4.cern.ch/share/data/RealSurface2.1.1
+export G4SAIDXSDATA=/cvmfs/geant4.cern.ch/share/data/G4SAIDDATA2.0
+export G4ABLADATA=/cvmfs/geant4.cern.ch/share/data/G4ABLA3.1
+export G4INCLDATA=/cvmfs/geant4.cern.ch/share/data/G4INCL1.0
+#cp -r $G4LEDATA/* ./geant4data; export G4LEDATA=$PWD/geant4data; 
 
 mkdir build install
 cd build
