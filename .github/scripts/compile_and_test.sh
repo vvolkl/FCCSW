872c2cd49f0b161930e225c9dc0a438c87fa631b
Valentin Volkl <valentin.volkl@cern.ch>
2020-07-12 21:27:12 +0200

    up

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -23,4 +23,5 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17  .. && \
 make  -j `getconf _NPROCESSORS_ONLN` && \
 make install && \
+ls -alhR $G4LEDATA && \
 ctest -j `getconf _NPROCESSORS_ONLN` --output-on-failure
